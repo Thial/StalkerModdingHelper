cd6b4a0e1e26db3c26faa206830ec0b81fe97589
Thial <thial9292@gmail.com>
2023-08-16 18:52:54 +0200

    Stalker Modding Helper Configurator
    - Added a new project for Stalker Modding Helper Configurator and restructured the solution. The configurator tool will be used to configure a more advanced config version based on json which will allow a lot finer control over the mods you are working on.

---
+++ StalkerModdingHelperConfigurator/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StalkerModdingHelperConfigurator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ StalkerModdingHelperConfigurator/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace StalkerModdingHelperConfigurator
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
